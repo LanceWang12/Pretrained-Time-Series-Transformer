e276db3c8af5b9a7a72ad3c19c606bf510ef3bf8
LanceWang12 <backfall52@gmail.com>
2023-08-06 10:17:00 +0000

    fuzzify output

--- main_SPCPatchBert.py
+++ main_SPCPatchBert.py
@@ -33,32 +33,34 @@ def parse() -> argparse.Namespace:
     parser.add_argument('--test_size', type=float, default=None)
     parser.add_argument('--params', type=int, default=1)
 
-    anomaly_tasks = ["DMDS"]
-    parser.add_argument('--task', type=str, default="DMDS",
+    anomaly_tasks = ["DMDS", "SWaT", "SMD"]
+    parser.add_argument('--task', type=str, required=True,
                         choices=anomaly_tasks)
 
     # -------- hyperparameter for SPCBERT --------
-    parser.add_argument('--model_name', type=str, default="SPCPatchBert")
+    parser.add_argument('--model_name', type=str, default="MultiSPCPatchBert")
     parser.add_argument('--window_size', type=int, default=128)
     parser.add_argument('--input_dim', type=int, default=32)
     parser.add_argument('--num_hidden_layers', type=int, default=3)
     parser.add_argument('--num_attention_heads', type=int, default=8)
     parser.add_argument('--hidden_size', type=int, default=128)
+    parser.add_argument('--intermediate_size', type=int, default=256)
     parser.add_argument('--attention_probs_dropout', type=int, default=0.1)
     parser.add_argument('--hidden_dropout_prob', type=int, default=0.1)
     parser.add_argument('--output_attention', type=int, default=1)
     parser.add_argument('--norm', type=int, default=1)
-    parser.add_argument('--spc_rule_num', type=int, default=1)
     parser.add_argument('--patch_len', type=int, default=16)
     parser.add_argument('--stride', type=int, default=8)
     parser.add_argument('--patch_padding', type=int, default=1)
-    parser.add_argument('--sensitive_level', type=float, default=20)
+    parser.add_argument('--ensemble', type=int, default=1)
+    # DMDS: 20, SWaT:
+    parser.add_argument('--sensitive_level', type=float, default=1)
     parser.add_argument('--alpha', type=float, default=0.5)
+    parser.add_argument('--load_norm', type=str, default="")
+    parser.add_argument('--load_embed', type=str, default="")
+    parser.add_argument('--load_encoder', type=str, default="")
+    parser.add_argument('--verbose', type=int, default=0)
     args = parser.parse_args()
-    print('=' * 70)
-    for key, value in vars(args).items():
-        print(f'{key}: {value}')
-    print('=' * 70)
 
     if args.task == "DMDS":
         args.input_dim = 15
@@ -91,6 +93,23 @@ def parse() -> argparse.Namespace:
         ]
         args.spc_head_lst = [5, 5, 6, 5, 2, 5, 4, 6, 3, 4, 4, 6, 6, 6, 6]
         args.spc_rule_num = len(args.spc_col)
+        # args.sensitive_level = 20
+    elif args.task == "SWaT":
+        args.input_dim = 30
+        # In SWaT dataset
+        # Train[380000: 440000], Val[450000: 470000], Test[478254: 498254]
+        args.spc_col = [
+            'FIT101_rule1', 'LIT101_rule1', 'MV101_rule1', 'P101_rule1', 'AIT203_rule1', 'FIT201_rule1', 'MV201_rule1', 'P203_rule1', 'P205_rule1', 'DPIT301_rule1', 'FIT301_rule1', 'LIT301_rule1', 'MV302_rule1', 'MV304_rule1', 'P302_rule1', 'AIT402_rule1', 'FIT401_rule1', 'LIT401_rule1', 'P402_rule1', 'UV401_rule1', 'AIT501_rule1', 'AIT502_rule1', 'FIT501_rule1', 'FIT502_rule1', 'FIT503_rule1', 'FIT504_rule1', 'P501_rule1', 'PIT501_rule1', 'PIT502_rule1', 'PIT503_rule1', 'P402_rule2', 'UV401_rule2', 'FIT502_rule2', 'P501_rule2', 'FIT101_rule3', 'LIT101_rule3', 'MV101_rule3', 'P101_rule3', 'AIT203_rule3', 'FIT201_rule3', 'MV201_rule3', 'P203_rule3', 'P205_rule3', 'DPIT301_rule3', 'FIT301_rule3', 'LIT301_rule3', 'MV302_rule3', 'MV304_rule3', 'P302_rule3', 'AIT402_rule3', 'FIT401_rule3', 'LIT401_rule3', 'P402_rule3', 'UV401_rule3', 'AIT501_rule3', 'AIT502_rule3', 'FIT501_rule3', 'FIT502_rule3', 'FIT503_rule3', 'FIT504_rule3', 'P501_rule3', 'PIT501_rule3', 'PIT502_rule3',
+            'PIT503_rule3', 'FIT101_rule4', 'MV101_rule4', 'P101_rule4', 'FIT201_rule4', 'MV201_rule4', 'P203_rule4', 'P205_rule4', 'DPIT301_rule4', 'FIT301_rule4', 'MV302_rule4', 'MV304_rule4', 'P302_rule4', 'FIT401_rule4', 'P402_rule4', 'UV401_rule4', 'FIT501_rule4', 'FIT502_rule4', 'FIT503_rule4', 'FIT504_rule4', 'P501_rule4', 'PIT502_rule4', 'FIT101_rule5', 'LIT101_rule5', 'P101_rule5', 'AIT203_rule5', 'FIT201_rule5', 'MV201_rule5', 'P203_rule5', 'P205_rule5', 'LIT301_rule5', 'AIT402_rule5', 'FIT401_rule5', 'LIT401_rule5', 'AIT501_rule5', 'AIT502_rule5', 'FIT501_rule5', 'FIT502_rule5', 'FIT503_rule5', 'FIT504_rule5', 'PIT501_rule5', 'PIT503_rule5', 'FIT101_rule6', 'MV101_rule6', 'P101_rule6', 'FIT201_rule6', 'MV201_rule6', 'P203_rule6', 'P205_rule6', 'DPIT301_rule6', 'FIT301_rule6', 'MV302_rule6', 'MV304_rule6', 'P302_rule6', 'FIT401_rule6', 'P402_rule6', 'UV401_rule6', 'FIT501_rule6', 'FIT502_rule6', 'FIT503_rule6', 'FIT504_rule6', 'P501_rule6', 'PIT501_rule6', 'PIT502_rule6', 'PIT503_rule6'
+        ]
+        args.spc_head_lst = [5, 3, 4, 5, 3, 5, 5, 5, 5, 4, 4, 3,
+                             4, 4, 4, 3, 5, 3, 5, 5, 3, 3, 5, 6, 5, 5, 5, 4, 4, 4]
+        args.spc_rule_num = len(args.spc_col)
+
+    print('=' * 70)
+    for key, value in vars(args).items():
+        print(f'{key}: {value}')
+    print('=' * 70)
 
     return args
 
@@ -102,6 +121,7 @@ def main() -> None:
     data_config = SPCAnomalyConfig(
         spc_col=args.spc_col,
         target_col="anomaly_label",
+        time_idx=None,
         window_size=args.window_size,
         batch_size=args.batch_size,
         test_size=args.test_size,
@@ -111,7 +131,7 @@ def main() -> None:
         echo=True,
     )
     train_loader, val_loader, thres_loader, test_loader = get_loader(
-        data_config, "DMDS")
+        data_config, args.task)
 
     # seq_len = original_seq_len + a spc token
     model_config = SPCBertConfig(
@@ -120,6 +140,7 @@ def main() -> None:
         window_size=args.window_size,
         spc_rule_num=args.spc_rule_num,
         hidden_size=args.hidden_size,
+        intermediate_size=args.intermediate_size,
         num_hidden_layers=args.num_hidden_layers,
         num_attention_heads=args.num_attention_heads,
         hidden_act="gelu",
@@ -131,7 +152,11 @@ def main() -> None:
         stride=args.stride,
         spc_head_lst=args.spc_head_lst,
         sensitive_level=args.sensitive_level,
-        verbose=False,
+        ensemble=args.ensemble,
+        load_norm=args.load_norm,
+        load_embed=args.load_embed,
+        load_encoder=args.load_encoder,
+        verbose=args.verbose,
     )
     net = SPCPatchBert(model_config)
 
